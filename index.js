7c514d830c8797fa314b1a3b0c6d0a4e240f51cf
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-10-21 00:31:00 -0400

    Input for GitHub repository added

--- index.js
+++ index.js
@@ -6,21 +6,34 @@ const inquirer = require('inquirer');
 const api = require('./utils/api.js');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// array of questions for user
+// Array of questions for user
 const questions = [
     // GitHub Username
     {
-       type: 'input',
-       name: 'username',
-       message: 'Enter your GitHub username.',
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username.',
         // We need to validate that user entered a word
         // https://stackoverflow.com/questions/57321266/how-to-test-inquirer-validation
         validate: function (answer) {
-           if (answer.length < 1) {
-               return console.log("You must enter a GitHub username.");
-           }
+            if (answer.length < 1) {
+                return console.log("You must enter a GitHub username.");
+            }
            return true;
         }
+    },
+    // GitHub Repository
+    {
+        type: 'input',
+        name: 'repository',
+        message: 'Enter the name of your repository on GitHub.',
+        // We need to validate that user entered a word
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("You must enter the name of your GitHub repository.");
+            }
+           return true;
+        } 
     },
 ];
 
