e92d511ec607be49287eb50d4d50d314ecc2a655
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-10-21 03:11:58 -0400

    Psuedo-coded generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,27 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(userResponses, userInfo) {
+  // Plug userReponses into table of contents
 
-`;
-}
+  // Generate badges
 
+  // Add table of contents data to markdown
+
+  // Add license section to markdown
+
+  // Create installation section
+
+  // Create usage section
+
+  // Create contribution section
+
+  // Create tests section
+
+  // Connect userResponses to license section
+
+  // Questions section
+
+  // Return markdown
+};
+
+// Export markdown module
 module.exports = generateMarkdown;
