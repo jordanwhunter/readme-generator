ee6890a708d6b233af9f3461882cd769a61d0bac
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-10-21 18:47:47 -0400

    Changed license positioning

--- index.js
+++ index.js
@@ -75,6 +75,15 @@ const questions = [
         name: 'usage',
         message: 'Enter your project instructions and examples of it in use for Usage Section.',
     },
+    // Select license
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Choose your license for your project.',
+        // https://docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository
+        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
+        
+    },
     // Contributing to project
     {
         type: 'input',
@@ -88,15 +97,6 @@ const questions = [
         name: 'tests',
         message: 'Provide tests for project, and explain how to test (if necessary).',
         // Validation not required if question is optional
-    },
-    // Select license
-    {
-        type: 'input',
-        name: 'license',
-        message: 'Choose your license for your project.',
-        // https://docs.github.com/en/free-pro-team@latest/github/creating-cloning-and-archiving-repositories/licensing-a-repository
-        choices: ['afl-3.0', 'apache-2.0', 'artistic-2.0', 'bsl-1.0', 'bsd-2-clause', 'bsd-3-clause', 'bsd-3-clause-clear', 'cc', 'cc0-1.0', 'cc-by-4.0', 'cc-by-sa-4.0', 'wtfpl', 'ecl-2.0', 'epl-1.0', 'epl-2.0', 'eupl-1.1', 'agpl-3.0', 'gpl', 'gpl-2.0', 'gpl-3.0', 'lgpl', 'lgpl-2.1', 'lgpl-3.0', 'isc', 'lppl-1.3c', 'ms-pl', 'mit', 'mpl-2.0', 'osl-3.0', 'postgresql', 'ofl-1.1', 'ncsa', 'unlicense', 'zlib']
-        
     },
 ];
 
