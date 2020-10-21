6596a1c477c0fcb7a920c4e8cbdb5cd035e2c505
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-10-21 18:35:01 -0400

    Created variable to draft md, added toc elements, and added toc to md

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,39 @@
+// GitHub formatting syntax for markdowns:
+// https://docs.github.com/en/free-pro-team@latest/github/writing-on-github/basic-writing-and-formatting-syntax
+
 // function to generate markdown for README
 function generateMarkdown(userResponses, userInfo) {
+
+  // Create title and description
+  // Generate badges
+  let draftMarkdown = 
+  `# ${userResponses.title}
+  (https://img.shields.io/github/languages/top/${userResponses.username}/${userResponses.repo}?style=flat&logo=appveyor)
+  
+  ## Description
+  ${userResponses.description}`
+
   // Plug userReponses into table of contents
   let draftTable = `## Table of Contents`;
-  // Generate badges
 
-  // Add table of contents data to markdown
-
+  if (userResponses.installation !== '') {
+    draftTable += `* [Installation] (#installation)`
+  };
+  if (userResponses.usage !== '') {
+    draftTable += `* [Usage] (#usage)`
+  };
   // Add license section to markdown
+  draftMarkdown += `* [License] (#License)`;
+  if (userResponses.contributing !== '') {
+    draftTable += `* [Contributing] (#contributing)`
+  };
+  if (userResponses.tests !== '') {
+    draftTable += `* [Tests] (#tests)`
+  };
+  
+  // Add table of contents data to markdown
+  draftMarkdown += draftTable;
+  
 
   // Create installation section
 
