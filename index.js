8dcec1698d91f47b923bf682c2eb7355c835dcf8
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-10-21 00:08:42 -0400

    Input for GitHub username added

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // array of questions for user
 const questions = [
-
+    // GitHub Username
+    {
+       type: 'input',
+       name: 'username',
+       message: 'Enter your GitHub username.',
+        // We need to validate that user entered a word
+        // https://stackoverflow.com/questions/57321266/how-to-test-inquirer-validation
+        validate: function (answer) {
+           if (answer.length < 1) {
+               return console.log("You must enter a GitHub username.");
+           }
+           return true;
+        }
+    },
 ];
 
 // function to write README file
